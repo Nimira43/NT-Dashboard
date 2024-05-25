0c9b93bf832315881b9cec90d056e3ea3089fad9
Nick Rabone <n.rabone72@aol.com>
2024-05-25 13:07:24 +0100

    Set up H tag global styles

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -2,14 +2,14 @@ import { Button } from "@/components/ui/button"
 
 export default function LandingPage() {
     return (
-        <div>
-            <h1>NT Dashboard</h1>
+        <>
+            <h1 className="">NT Dashboard</h1>
             <Button>
                 Log In
             </Button>
             <Button>
                 Sign Up
             </Button>
-        </div>
+        </>
     )
 }
