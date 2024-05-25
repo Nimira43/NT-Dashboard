f435c351b1ee0c444b7405d8fb8738543afb5ff3
Nick Rabone <n.rabone72@aol.com>
2024-05-25 14:57:13 +0100

    Customised button.tsx and global.css

--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@/components/ui/button"
+import { Satellite } from "lucide-react"
+import Link from "next/link"
 
 export default function LandingPage() {
     return (
         <>
-            <h1 className="">NT Dashboard</h1>
-            <Button>
-                Log In
-            </Button>
-            <Button>
-                Sign Up
-            </Button>
+            <h1 className="flex gap-2 items-center">
+                <Satellite size={50} className="text-[#ea580c]"/> 
+                NT Dashboard
+            </h1>
+            <p>Project Management Support Dashboard</p>
+            <div className="flex gap-2 items-center">
+                <Button asChild >
+                    <Link href="/login">Login</Link>
+                </Button>
+                <small>or</small>
+                <Button asChild variant="outline">
+                    <Link href="/signup">Sign Up</Link>    
+                </Button>
+            </div> 
         </>
     )
 }
