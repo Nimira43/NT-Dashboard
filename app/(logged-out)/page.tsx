a7bffce7e90cb6aa3df5c98075ffa6a7d20c8900
Nick Rabone <n.rabone72@aol.com>
2024-05-25 12:29:48 +0100

    Working on font and bringing in Shadcn-ui

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+export default function LandingPage() {
+    return (
+        <div>
+            <h1>NT Dashboard</h1>
+            <Button>
+                Log In
+            </Button>
+            <Button>
+                Sign Up
+            </Button>
+        </div>
+    )
+}
