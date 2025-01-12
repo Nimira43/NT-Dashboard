8e8ed64d83a2185bae41578f835730da88273c28
Nimira43 <n.rabone72@aol.com>
2025-01-12 04:52:21 +0000

    Adding more styling to dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,9 +2,9 @@ export default function DashboardLayout(
   {children}: {children: React.ReactNode}
 ) {
   return (
-    <div className='grid grid-cols-[250px_1fr]'>
-      <div>Side Panel</div>
-      <div>Main Dashboard</div>
+    <div className='grid grid-cols-[250px_1fr] h-screen'>
+      <div className='bg-[#ff4500] text-[#111] overflow-auto'>sidebar</div>
+      <div className='overflow-auto'>{children}</div>
     </div>
   )
 }
