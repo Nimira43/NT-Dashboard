3143834c5d1e5f5ac2f29638b666bd4530d09c09
Nimira43 <n.rabone72@aol.com>
2025-01-12 04:39:44 +0000

    Created layout.tsx for dashboard. Setup basic dashboard layout.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+export default function DashboardLayout(
+  {children}: {children: React.ReactNode}
+) {
+  return (
+    <div className='grid grid-cols-[250px_1fr]'>
+      <div>Side Panel</div>
+      <div>Main Dashboard</div>
+    </div>
+  )
+}
