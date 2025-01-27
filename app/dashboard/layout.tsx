ff44ec1da4cd1d020da3df52189b637e0d1e36e0
Nimira43 <n.rabone72@aol.com>
2025-01-27 21:07:02 +0000

    Add base styling to Dashboard.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,8 +3,11 @@ export default function DashboardLayout(
 ) {
   return (
     <div className='grid grid-cols-[250px_1fr] h-screen'>
-      <div className='bg-[#ff4500] text-[#111] overflow-auto'>sidebar</div>
-      <div className='overflow-auto'>{children}</div>
+      <div className='bg-[#000] overflow-auto p-4'>sidebar</div>
+      <div className='overflow-auto py-2 px-4'>
+        <h1 className='pb-4'>Monday 27th January 2025</h1>
+        {children}
+      </div>
     </div>
   )
 }
