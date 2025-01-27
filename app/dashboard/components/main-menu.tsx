5db823858dd18d9fdc30aeb8287d289efe9e8d19
Nimira43 <n.rabone72@aol.com>
2025-01-27 21:46:54 +0000

    Created new MenuTitle component.

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -1,3 +1,12 @@
+import MenuTitle from './menu-title'
+
 export default function MainMenu() {
-  return <div className='bg-[#000] overflow-auto p-4'>sidebar</div>
+  
+  return (
+    <div className='bg-[#000] overflow-auto p-4'>
+      <div className='border-b border-b-[#ea580c] pb-4'>
+        <MenuTitle />
+      </div>
+    </div>
+  )
 }
---
+++ app/dashboard/components/menu-title.tsx
@@ -0,0 +1,13 @@
+import { Satellite } from "lucide-react";
+
+export default function MenuTitle() {
+  return (
+    <h4 className='flex items-center'>
+      <Satellite 
+        size={40} 
+        className='text-[#ea580c]'
+      /> Dashboard
+
+    </h4>
+  )
+}
