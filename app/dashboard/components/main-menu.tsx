3c262e5da9570dda6ba2c6dc2534086f5473542e
Nimira43 <n.rabone72@aol.com>
2025-01-27 21:15:21 +0000

    Created MainMenu component that'll be used for large and small screens

---
+++ app/dashboard/components/main-menu.tsx
@@ -0,0 +1,3 @@
+export default function MainMenu() {
+  return <div className='bg-[#000] overflow-auto p-4'>sidebar</div>
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,11 @@
+import MainMenu from './components/main-menu'
+
 export default function DashboardLayout(
   {children}: {children: React.ReactNode}
 ) {
   return (
     <div className='grid grid-cols-[250px_1fr] h-screen'>
-      <div className='bg-[#000] overflow-auto p-4'>sidebar</div>
+      <MainMenu/>
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'>Monday 27th January 2025</h1>
         {children}
