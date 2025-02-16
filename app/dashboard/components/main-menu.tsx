11ea58b5b871c0e0e3daa279c2016bf8d796026b
Nimira43 <n.rabone72@aol.com>
2025-02-16 00:43:51 +0000

    Improving the styling of the border bottom under the MenuTitle in main-menu.tsx.

--- app/dashboard/components/main-menu.tsx
+++ app/dashboard/components/main-menu.tsx
@@ -5,10 +5,10 @@ export default function MainMenu() {
   
   return (
     <div className='bg-[#000] overflow-auto p-4'>
-      <div className='border-b border-b-[#ea580c] pb-4'>
+      <div className='border-b dark:border-b-[#fff] border-b-[#ea580c] pb-4'>
         <MenuTitle />
       </div>
-      <div>
+      <div className='py-4'>
         <MenuItem href='/dashboard'>
           Dashboard
         </MenuItem>
