14e7b0ee9bdcfc9c71009ca4e9d98dd93d6651a1
Nimira43 <n.rabone72@aol.com>
2025-05-11 15:17:11 +0100

    Added border to Card 3. Added CardHeader and CardTitle to Card 1

--- app/dashboard/components/employees-stats.tsx
+++ app/dashboard/components/employees-stats.tsx
@@ -1,15 +1,19 @@
-import { Card } from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function EmployeesStats() {
   return (
     <div className='grid lg:grid-cols-3 gap-4'>
       <Card>
-        Card 1
+        <CardHeader>
+          <CardTitle className='font-normal text-base'>
+            Total Employees
+          </CardTitle>
+        </CardHeader>
       </Card>
       <Card>
         Card 2
       </Card>
-      <Card>
+      <Card className='border-main'>
         Card 3
       </Card>
     </div>
