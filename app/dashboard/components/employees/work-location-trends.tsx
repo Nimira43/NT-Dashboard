698bf8fbb65bb8f8544f80a57db8d4d4aeec3570
Nimira43 <n.rabone72@aol.com>
2025-06-17 11:23:01 +0100

    Rendering the bar chart

--- app/dashboard/components/employees/work-location-trends.tsx
+++ app/dashboard/components/employees/work-location-trends.tsx
@@ -1,9 +1,19 @@
 'use client'
 
-import { } from 'recharts'
+import { Bar, BarChart, ResponsiveContainer } from 'recharts'
 import { data } from './employee-attending-data'
+
 export default function WorkLocationTrends() {
   return (
-    <div>Work Location Trends</div> 
+    <ResponsiveContainer 
+      height={350}
+      width='100%'
+    >
+      <BarChart data={data}>
+        <Bar dataKey='office' stackId={1} fill='#ff4500' />
+        <Bar dataKey='wfh' stackId={1} fill='#ffd700' />
+      </BarChart>
+
+    </ResponsiveContainer> 
   )
 }
