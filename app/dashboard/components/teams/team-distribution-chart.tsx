55d2def8a0455da888ceb86f2281098c4dd4d6e7
Nimira43 <n.rabone72@aol.com>
2025-07-25 00:43:36 +0100

    Created dummy data array.

--- app/dashboard/components/teams/team-distribution-chart.tsx
+++ app/dashboard/components/teams/team-distribution-chart.tsx
@@ -1,4 +1,21 @@
 export default function TeamDistributionChart() {
+  const data= [
+    {
+      name: 'Eagles',
+      value: 47,
+      colour: ''
+    },
+    {
+      name: 'Lions',
+      value: 34,
+      colour: ''
+    },
+    {
+      name: 'Rhinos',
+      value: 19,
+      colour: ''
+    }
+  ]
   return (
     <div>TeamDistributionChart</div>
   )
