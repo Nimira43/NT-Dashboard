e2bb2aa55a3183567b956dfc9bd5e796a17394fe
Nimira43 <n.rabone72@aol.com>
2025-08-10 03:53:09 +0100

    Legend

--- app/dashboard/components/teams/support-tickets-resolved.tsx
+++ app/dashboard/components/teams/support-tickets-resolved.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
 import { ticketsData } from './teams-data'
 
 export default function SupportTicketsResolved() {
@@ -15,9 +15,11 @@ export default function SupportTicketsResolved() {
         <XAxis 
           fontSize={12} 
           dataKey='name' 
+          stroke='#888888'
         />
         <YAxis 
           fontSize={12} 
+          stroke='#888888'
         />
         <CartesianGrid 
           strokeDasharray='3' 
@@ -36,6 +38,16 @@ export default function SupportTicketsResolved() {
           dataKey='rhinos' 
           type='monotone'
           stroke='#00ff00'
+        />
+        <Legend 
+          formatter={
+            (value) => 
+            <span
+              className='capitalize'
+            >
+              {value}
+            </span>
+          }
         />
       </LineChart>
     </ResponsiveContainer>
