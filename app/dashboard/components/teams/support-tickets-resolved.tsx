c7d42a5d5280f08d356da836caf0c979fbbc4af8
Nimira43 <n.rabone72@aol.com>
2025-08-10 03:21:10 +0100

    Working on linechart

--- app/dashboard/components/teams/support-tickets-resolved.tsx
+++ app/dashboard/components/teams/support-tickets-resolved.tsx
@@ -1,10 +1,33 @@
 'use client'
 
+import { Line, LineChart, ResponsiveContainer } from 'recharts'
 import { ticketsData } from './teams-data'
 
 export default function SupportTicketsResolved() {
   return (
-    <div>Tickets</div>
+    <ResponsiveContainer
+      height={350}
+      width='100%'
+    >
+      <LineChart data={ticketsData}>
+        <Line />
+      </LineChart>
+
+    </ResponsiveContainer>
   )
 }
-console.log(ticketsData)
+// {
+//       name: 'Eagles',
+//       value: 31,
+//       colour: '#ff4500'
+//     },
+//     {
+//       name: 'Lions',
+//       value: 39,
+//       colour: '#ffd700'
+//     },
+//     {
+//       name: 'Rhinos',
+//       value: 30,
+//       colour: '#00ff00'
+//     }
