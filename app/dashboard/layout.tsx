3eb96cea178697f6dfbac4cc6b8e45624510dead
Nimira43 <n.rabone72@aol.com>
2025-08-16 12:53:19 +0100

    Responsiveness - 4

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import MainMenu from './components/main-menu'
+import MenuTitle from './components/menu-title'
 
 export default function DashboardLayout(
   {children}: {children: React.ReactNode}
@@ -8,8 +9,8 @@ export default function DashboardLayout(
       <MainMenu 
         className='hidden md:flex'
       />
-      <div className='block md:hidden sticky top-0 left-0 bg-background'>
-        mobile menu
+      <div className='p-4block md:hidden sticky top-0 left-0 bg-background border-b border-border'>
+        <MenuTitle />
       </div>
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'>Tuesday 14th May 2025</h1>
