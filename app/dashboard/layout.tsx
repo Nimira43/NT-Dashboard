8ae32fdd6889fcf2f85e01e9389ccba6b19d7c7d
Nimira43 <n.rabone72@aol.com>
2025-08-16 13:05:11 +0100

    Responsiveness-6

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
+import { Drawer, DrawerTrigger } from '@/components/ui/drawer'
 import MainMenu from './components/main-menu'
 import MenuTitle from './components/menu-title'
+import { HiMenu } from 'react-icons/hi'
 
 export default function DashboardLayout(
   {children}: {children: React.ReactNode}
@@ -11,6 +13,11 @@ export default function DashboardLayout(
       />
       <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
         <MenuTitle />
+        <Drawer>
+          <DrawerTrigger>
+            <HiMenu />
+          </DrawerTrigger>
+        </Drawer>
       </div>
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'>Tuesday 14th May 2025</h1>
