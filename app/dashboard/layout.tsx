b6063f6c4be52c92272d77bc02ed46567069b657
Nimira43 <n.rabone72@aol.com>
2025-08-16 12:39:14 +0100

    Updating Dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,8 +4,11 @@ export default function DashboardLayout(
   {children}: {children: React.ReactNode}
 ) {
   return (
-    <div className='grid grid-cols-[250px_1fr] h-screen'>
-      <MainMenu/>
+    <div className='grid md:grid-cols-[250px_1fr] h-screen'>
+      <MainMenu />
+      <div className='sticky top-0 left-0 bg-background'>
+        mobile menu
+      </div>
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'>Tuesday 14th May 2025</h1>
         {children}
