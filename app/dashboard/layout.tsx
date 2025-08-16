6c886388dd764eb2b7643cd1c220018cf6e93fa2
Nimira43 <n.rabone72@aol.com>
2025-08-16 13:17:56 +0100

    Responsiveness - 8

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,26 +2,34 @@ import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
 import MainMenu from './components/main-menu'
 import MenuTitle from './components/menu-title'
 import { HiMenu } from 'react-icons/hi'
+import { useMediaQuery } from '@/hooks/use-media-query'
 
-export default function DashboardLayout(
-  {children}: {children: React.ReactNode}
-) {
+export default function DashboardLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  const isDesktop = useMediaQuery('min-width(768px)')
+
   return (
     <div className='grid md:grid-cols-[250px_1fr] h-screen'>
       <MainMenu 
         className='hidden md:flex'
       />
-      <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
-        <MenuTitle />
-        <Drawer>
-          <DrawerTrigger>
-            <HiMenu />
-          </DrawerTrigger>
-          <DrawerContent>
-            <MainMenu />
-          </DrawerContent>
-        </Drawer>
-      </div>
+      {!isDesktop && (
+        <div className='p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border'>
+          <MenuTitle />
+          <Drawer>
+            <DrawerTrigger>
+              <HiMenu />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
+      
       <div className='overflow-auto py-2 px-4'>
         <h1 className='pb-4'>Tuesday 14th May 2025</h1>
         {children}
