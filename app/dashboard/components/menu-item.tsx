b5a7c0331744ed4adfca6ea1f940c88bb8dbfba8
Nimira43 <n.rabone72@aol.com>
2025-09-08 22:49:20 +0100

    Update MenuItem

--- app/dashboard/components/menu-item.tsx
+++ app/dashboard/components/menu-item.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { DrawerContext } from '@/components/ui/drawer'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useContext } from 'react'
 
 type Props = {
   children: React.ReactNode,
@@ -10,6 +12,7 @@ type Props = {
 }
 
 export default function MenuItem({ children, href}: Props) {
+  const { onClose } = useContext(DrawerContext)
   const pathname = usePathname()
   const isActive = pathname === href
 
@@ -21,6 +24,7 @@ export default function MenuItem({ children, href}: Props) {
         'bg-primary text-light hover:bg-light dark:hover:bg-dark '
       )}
       href={href}
+      onClick={onClose}
     >
       {children}
     </Link>
