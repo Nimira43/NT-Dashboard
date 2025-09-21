6ad6690f18ec3532f3b0cbf7669f2cb0bfe4b5eb
Nimira43 <n.rabone72@aol.com>
2025-09-21 19:13:58 +0100

    Loading - 2

--- app/dashboard/employees/loading.tsx
+++ app/dashboard/employees/loading.tsx
@@ -1,9 +1,13 @@
-import { Card } from '@/components/ui/card'
+import { Card, CardHeader, CardTitle } from '@/components/ui/card'
 
 export default function Loading() {
   return (
     <Card>
-
+      <CardHeader>
+        <CardTitle>
+          Employees
+        </CardTitle>
+      </CardHeader>
     </Card>
   )
 }
