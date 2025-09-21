0d749b49c0b98a4b6c949fb8c4d2f261c903bd12
Nimira43 <n.rabone72@aol.com>
2025-09-21 19:35:07 +0100

    Loading - 4

--- app/dashboard/employees/loading.tsx
+++ app/dashboard/employees/loading.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export default function Loading() {
   return (
@@ -9,9 +10,23 @@ export default function Loading() {
         </CardTitle>
       </CardHeader>
       <CardContent 
-        className='grid grid-cols-[60px_1fr_1fr_1fr_1fr]'
+        className='grid grid-cols-[60px_1fr_1fr_1fr_1fr] gap-4 items-center'
       >
-
+        <Skeleton 
+          className='size-10 rounded-full ' 
+        />
+        <Skeleton 
+          className='h-8 w-full' 
+        />
+        <Skeleton 
+          className='h-8 w-full' 
+        />
+        <Skeleton 
+          className='h-8 w-full' 
+        />
+        <Skeleton 
+          className='h-8 w-full' 
+        />
       </CardContent>
     </Card>
   )
