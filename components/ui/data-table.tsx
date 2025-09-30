b61712d38603d458c37030cfd67b28a7c586aa14
Nimira43 <n.rabone72@aol.com>
2025-09-30 19:15:59 +0100

    Importing Table component

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -1,3 +1,5 @@
 'use-client'
 
 import { ColumnDef, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
+
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
