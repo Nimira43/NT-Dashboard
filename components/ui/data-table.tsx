8d998abb04852a1ac33bd68fe8ffa3a74a3a2302
Nimira43 <n.rabone72@aol.com>
2025-09-30 19:20:25 +0100

    DataTableProps interface

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -3,3 +3,8 @@
 import { ColumnDef, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
