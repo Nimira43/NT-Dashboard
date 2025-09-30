3f08e87e81bbe175a6036da9b70c4ecbfd924611
Nimira43 <n.rabone72@aol.com>
2025-09-30 19:23:04 +0100

    DataTable props and types

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -8,3 +8,10 @@ interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
   data: TData[]
 }
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+
+}
