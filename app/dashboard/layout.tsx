86a7e6de4efc4b5c9556958c240fde60f49a69ab
Nimira43 <n.rabone72@aol.com>
2025-10-14 02:20:26 +0100

    Replaced hard-coded date with formattedDate.

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
 }) {
   const isDesktop = useMediaQuery('(min-width: 768px)')
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false) 
-  
+
   const today = new Date()
   const formattedDate = today.toLocaleDateString('en-GB', {
     weekday: 'long',
@@ -48,7 +48,7 @@ export default function DashboardLayout({
       )}
       
       <div className='overflow-auto py-2 px-4'>
-        <h1 className='pb-4'>Tuesday 14th May 2025</h1>
+        <h1 className='pb-4'>{formattedDate}</h1>
         {children}
       </div>
     </div>
